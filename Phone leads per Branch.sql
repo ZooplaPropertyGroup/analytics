ec4de42b26c9108905be48d55a763ddfa83c3dca
steven-tran <steven.tran@zpg.co.uk>
2016-09-15 12:24:10 +0100

    Date and Package Type change
    Changed call_start date to >= '2016-01-01'
    Changed package type from commercial to residential

--- Phone leads per Branch.sql
+++ Phone leads per Branch.sql
@@ -3,19 +3,19 @@
 --Original
 
 SELECT 
-  date_part(y,call_start) as year, 
-  date_part(mon,call_start) as month, 
+  date_part(y,call_start) as call_start_year, 
+  date_part(mon,call_start) as call_start_month, 
   l.branch_id, 
   count(distinct id) as total_phone_leads
 FROM agent_phone_leads_nopii as l
 WHERE 
-  call_start>='2015-07-01' 
+  call_start>='2016-01-01' 
   AND branch_id in 
     ( SELECT c.branch_id 
       FROM 
         contracts_migration as c
         INNER JOIN agent_packages as p on p.agent_package_id=c.agent_package_id
-      WHERE package_type='commercial'
+      WHERE package_type='residential'
     )
 GROUP BY 1,2,3
 ORDER BY 1,2,3
@@ -24,18 +24,19 @@ LIMIT 100
 --Edited
 
 SELECT 
-  DATEPART(YEAR, l.call_start) AS year, DATEPART(MONTH, l.call_start) AS month, 
+  DATEPART(YEAR, l.call_start) AS call_start_year, 
+  DATEPART(MONTH, l.call_start) AS call_start_month, 
   l.branch_id, 
   COUNT(DISTINCT l.id) AS total_phone_leads
 FROM agent_phone_leads_nopii AS l
 WHERE 
-  l.call_start >= '2015-07-01' 
+  l.call_start >= '2016-01-01' 
   AND l.branch_id IN 
     ( SELECT c.branch_id 
       FROM 
         contracts_migration AS c
         INNER JOIN agent_packages AS p ON p.agent_package_id = c.agent_package_id
-      WHERE p.package_type = 'commercial'
+      WHERE p.package_type = 'residential'
     )
 GROUP BY 1,2,3
 ORDER BY 1,2,3
